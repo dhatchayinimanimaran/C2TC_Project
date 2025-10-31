83ce07c28cd39cdd8c0ca3ee66018900337c8bbd
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-31 12:20:29 +0530

    commit

---
+++ OnlineShopping_Platform/src/com_tnsif_onlineshopping/entities/User.java
@@ -0,0 +1,19 @@
+package com_tnsif_onlineshopping.entities;
+
+public abstract class User {
+    protected final int userId;
+    protected String username;
+    protected String email;
+
+    public User(int userId, String username, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+
+    public int getUserId() { return userId; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
