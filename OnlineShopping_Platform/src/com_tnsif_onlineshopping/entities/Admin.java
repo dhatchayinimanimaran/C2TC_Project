8c96633e1430a9b2bd97ec92ed751445943ec03d
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-31 12:13:39 +0530

    commit

---
+++ OnlineShopping_Platform/src/com_tnsif_onlineshopping/entities/Admin.java
@@ -0,0 +1,14 @@
+package com_tnsif_onlineshopping.entities;
+
+public class Admin extends User {
+    public Admin(int userId, String username, String email) {
+        super(userId, username, email);
+    }
+
+    @Override
+    public String toString() {
+        return "Admin [userId=" + userId +
+               ", username=" + username +
+               ", email=" + email + "]";
+    }
+}
